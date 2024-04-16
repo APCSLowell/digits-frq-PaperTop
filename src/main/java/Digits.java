41a68b2fe19f87e003018c09b5ac1a93ad4db95b
PaperTop <135560395+PaperTop@users.noreply.github.com>
2024-04-15 17:45:48 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -11,18 +11,15 @@ public class Digits
 	    if (num == 0){
 		    digitList.add(0);
 	    } 
-	    while (num > 0){
-		    digitList.add(0, num%10);
-		    num = num/10;
-		}
+	    int temp = num
+	    while (temp > 0){
+		    digitList.add(0, temp%10);
+		    temp = temp/10;
 	    }
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		if (digitList.size() == 1){
-			return true;
-		} 
 		for (int i = 0; i < digitList.size() - 1; i++){
 			if (digitList.get(i) >= digitList.get(i+1)){
 				return false;
