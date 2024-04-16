635db6c638a21d224e2492ab23a34098d4c1526d
PaperTop <135560395+PaperTop@users.noreply.github.com>
2024-04-15 17:50:41 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -11,7 +11,7 @@ public class Digits
 	    if (num == 0){
 		    digitList.add(0);
 	    } 
-	    while (num > 0){
+	    while (num != 0){
 		    digitList.add(0, num%10);
 		    num = num/10;
 	    }
